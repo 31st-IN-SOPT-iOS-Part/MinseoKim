fe30e7f1da0422d8fce9d8ddec2357cd05a6f301
minseo205 <rlaalstj312@naver.com>
2022-10-06 21:46:30 +0900

    .gitignore

---
+++ week1_assignment/week1_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week1_assignment
+//
+//  Created by 김민서 on 2022/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
